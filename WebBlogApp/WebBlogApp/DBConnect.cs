232f5855c50df8d498ca6be91d46a31d2c9eaa81
Martins Davis Bernhards <49512684+mdbernhards@users.noreply.github.com>
2020-11-05 10:29:45 +0200

    Added DB connection to the API

---
+++ WebBlogApp/WebBlogApp/DBConnect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace WebBlogApp
+{
+    public class DBConnect
+    {
+        public void ConnectToDB()
+        {
+            try
+            {
+                // Connect to SQL
+                Console.Write("Connecting to SQL Server ... ");
+                using (SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\ProjectsV13;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+                {
+                    connection.Open();
+                    Console.WriteLine("Done.");
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("All done. Press any key to finish...");
+            Console.ReadKey(true);
+        }
+    }
+}

75ac9f609ebcfbabf07bde96ea7c0d34614bd628
Martins Davis Bernhards <49512684+mdbernhards@users.noreply.github.com>
2020-11-05 12:44:14 +0200

    Added login screen in vue

---
+++ BlogVueJs/blogvue/src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div>   
+            <h1>Blog</h1>
+            <h3>Login</h3>
+        </div>
+        <form action="">
+            <label for="fname">First name: </label>
+            <input type="text" id="fname" name="fname"><br><br>
+            <label for="lname">Last name: </label>
+            <input type="text" id="lname" name="lname"><br><br>
+            <input type="submit" value="Login">
+        </form>     
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
